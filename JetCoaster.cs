0987e49eb37122d2d0a1bd265624591cc7c7a4d9
日野　雅貴 <190796@jc-21.jp>
2022-02-04 14:06:30 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class JetCoaster
+    {
+
+        public static int ride(int k,int[] g, int r)//k=最大乗客数,g=グループごとの人数,r=乗る回数
+        {
+            int maxRide = k;
+            int gPeek;
+            int count=1;
+            int result=0;
+
+            for (int j=0; j<g.Length; j++)
+            {
+                gPeek = g[j];
+                Console.WriteLine(count+":"+gPeek + ":" + result);
+                maxRide -= gPeek;
+
+                if (maxRide >= 0) 
+                {
+                    result += gPeek;
+                    if (j == g.Length - 1)
+                    {
+                        j = -1;
+                    }
+                    continue;
+                }
+                else
+                {
+                    maxRide = k;
+                    j--;
+                    count++;
+                    if (count > r)
+                        break;
+                }    
+            }
+            Console.WriteLine("result:" + result);
+            return result;
+        }
+    }
+}
+/*
+ 1.先頭のグループを乗せる
+ 2.残りの座席数を確認する
+ 3.座席が余ってたら次のグループも乗せる
+ 4.座席が余ってなかったら出発する
+ 5.乗り終わったグループは列の最後尾に並ぶ
+ 6.n回繰り返す
+*/
---
+++ kuriage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class kuriage
+    {
+        public static int kuriage10(int i)
+        {
+            if (i == 0)
+            {
+                return 0;
+            }
+            else if (i >= 1)
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Ceiling(j / 10) * 10);
+                return i;
+            }
+            else
+            {
+                double j = i;
+                i = Convert.ToInt32(Math.Floor(j / 10) * 10);
+                return i;
+            }
+        }
+    }
+}
