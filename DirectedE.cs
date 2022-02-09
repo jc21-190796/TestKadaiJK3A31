48addd09e5f87de82ff471383b4256b43b54ca46
日野　雅貴 <190796@jc-21.jp>
2022-02-09 15:59:28 +0900

    DiretedEの作成完了

--- DirectedE.cs
+++ DirectedE.cs
@@ -8,18 +8,19 @@ namespace WindowsFormsApp1
 {
     public class DirectedE
     {
-        public static int SEARCH(int[] first,int[] next,int dep)
+        public static int[] SEARCH(int[] first, int[] next, int dep)
         {
             int temp = 0;
             int n = first.Length;
             int m = next.Length;
-            int[] start = new int[first.Length];
-            int[] end = new int[first.Length];
+            int[] start = { 1, 2, 3, 4, 2, 5, 4, 6 };
+            int[] end = { 2, 3, 4, 1, 5, 4, 6, 2 };
             int[] current = new int[first.Length];
             int[] searchd = new int[next.Length];
             int[] path = new int[next.Length];
 
-            for (int i = 0; i < first.Length; i++){
+            for (int i = 0; i < first.Length; i++)
+            {
                 current[i] = first[i];
             }
 
@@ -29,25 +30,28 @@ namespace WindowsFormsApp1
 
             while (last >= 1)
             {
-                if (current[x] != 0)
+                Console.WriteLine("current:" + current[x - 1]);
+                if (current[x - 1] != 0)
                 {
-                    temp = current[x];
-                    searchd[top] = temp;
-                    current[x] = next[temp];
-                    x = end[temp];
+                    temp = current[x - 1];
+                    searchd[top - 1] = temp;
+                    current[x - 1] = next[temp - 1];
+                    x = end[temp - 1];
                     top++;
                 }
                 else
                 {
                     top--;
-                    temp = searchd[top];
-                    path[last] = temp;
-                    x = start[temp];
+                    Console.WriteLine("top:" + top);
+                    temp = searchd[top - 1];
+                    path[last - 1] = temp;
+                    Console.WriteLine("path:" + path[last - 1]);
+                    x = start[temp - 1];
                     last--;
                 }
             }
 
-            return path[path.Length-1];
+            return path;
         }
     }
 }
